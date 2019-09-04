2ca4dfa7dd70dfbe330a9529c85a1caf31ae654d
daniele_leao <dani.leao89@gmail.com>
2019-09-03 22:04:44 -0300

    React wit redux

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,34 +1,48 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
 
 import {
-  Navbar, NavItem, SideNav, SideNavItem, Button, Icon, Row, Col,
-} from 'react-materialize';
+  Navbar,
+  NavItem,
+  SideNav,
+  SideNavItem,
+  Button,
+  Icon,
+  Row,
+  Col
+} from 'react-materialize'
 
-import './Menu.css';
-import Routes from '../../routers/routes';
+import './Menu.css'
+import Routes from '../../routers/routes'
 
 export default class Menu extends Component {
   state = {
-    openSide: false,
-  };
+    openSide: false
+  }
 
-  handleSidebar = (e) => {
-    e.preventDefault();
-    this.setState({ openSide: !this.state.openSide });
-  };
+  handleSidebar = e => {
+    e.preventDefault()
+    this.setState({ openSide: !this.state.openSide })
+  }
 
-  render() {
-    const { openSide } = this.state;
+  render () {
+    const { openSide } = this.state
 
     return (
       <Fragment>
         <Navbar style={{ background: '#41715f' }}>
           <NavItem>
-            <Button id="icon_menu" onClick={this.handleSidebar} floating waves="yellow">
+            <Button
+              id='icon_menu'
+              onClick={this.handleSidebar}
+              floating
+              waves='yellow'
+            >
               <Icon>menu</Icon>
             </Button>
           </NavItem>
-          <NavItem onClick={() => console.log('test click')}>PS - Front</NavItem>
+          <NavItem onClick={() => console.log('test click')}>
+            PS - Front
+          </NavItem>
 
           <NavItem style={{ float: 'right' }}>
             <Icon>account_circle</Icon>
@@ -45,16 +59,20 @@ export default class Menu extends Component {
           </SideNavItem>
           <SideNavItem divider />
           <SideNavItem subheader>Cadastro</SideNavItem>
-          <SideNavItem waves href="/cartao">
+          <SideNavItem waves href='/cartao'>
             Cartão
           </SideNavItem>
+          <Button>Cartão</Button>
         </SideNav>
+        <Button waves='light' node='a' href='/cartao'>
+          Acessar Cartão
+        </Button>
         <Row>
-          <Col s={12} className="grid-example">
+          <Col s={12} className='grid-example'>
             <Routes />
           </Col>
         </Row>
       </Fragment>
-    );
+    )
   }
 }
